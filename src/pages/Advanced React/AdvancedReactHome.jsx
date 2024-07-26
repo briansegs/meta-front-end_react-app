99ca03eac25e8473e97bd3336997db52e537623f
bsegers <briansegs@gmail.com>
2024-07-26 15:10:32 -0400

    Add content / styles

--- src/pages/Advanced React/AdvancedReactHome.jsx
+++ src/pages/Advanced React/AdvancedReactHome.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+const learningList = [
+  "1. Context Api for global State",
+  "2. Hooks: useRef useState useEffect",
+  "3. Controlled forms",
+  "4. State managment",
+  "5. Data fetching",
+  "6. Custom Hooks",
+];
+
+const InfoCard = ({ title, content }) => (
+  <div className="m-8 w-fit rounded-lg border-[1px] border-white bg-slate-50">
+    <div className="flex items-center rounded-t-lg px-8 py-6 justify-center w-full bg-gradient-to-r  from-blue-400 to-cyan-300">
+      <p className="text-white text-3xl font-semibold">{title}</p>
+    </div>
+    <div className="p-4 flex flex-col">{content}</div>
+  </div>
+);
+
 const AdvancedReactHome = () => {
-  return <div>AdvancedReactHome</div>;
+  return (
+    <section className="w-full bg-slate-200">
+      <div className="flex items-center w-full bg-slate-50 p-6">
+        <h1 className="text-3xl font-medium text-slate-600">Advanced React</h1>
+      </div>
+
+      <div className="w-full flex justify-center">
+        <InfoCard
+          title="First steps"
+          content={
+            <>
+              <p className="text-neutral-600 font-semibold text-xl">
+                Key learnings:
+              </p>
+              {learningList.map((idea) => (
+                <p key={idea} className="text-slate-600 pl-2">
+                  {idea}
+                </p>
+              ))}
+            </>
+          }
+        />
+      </div>
+    </section>
+  );
 };
 
 export default AdvancedReactHome;
