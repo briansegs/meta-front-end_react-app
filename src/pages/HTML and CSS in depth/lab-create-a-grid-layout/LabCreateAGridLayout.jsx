0256fbdf7b39c352900375f8b616790b1b0405ee
bsegers <briansegs@gmail.com>
2024-07-26 19:17:45 -0400

    update styles

--- src/pages/HTML and CSS in depth/lab-create-a-grid-layout/LabCreateAGridLayout.jsx
+++ src/pages/HTML and CSS in depth/lab-create-a-grid-layout/LabCreateAGridLayout.jsx
@@ -2,10 +2,10 @@ import styles from "./projectStyles.module.css";
 
 const LabCreateAGridLayout = () => {
   return (
-    <div className="w-full">
-      <div className={styles.container}>
+    <div className="w-full flex justify-center px-4 py-2 bg-slate-200">
+      <div className={`${styles.container} w-[700px] h-[500px]`}>
         <header className={styles.header}> Header </header>
-        <main className={styles.main}> Content </main>
+        <main className={`${styles.main} bg-white`}> Content </main>
         <div className={`${styles.sidebar} ${styles.left}`}> Sidebar </div>
         <div className={`${styles.sidebar} ${styles.right}`}> Sidebar </div>
         <footer class={styles.footer}> Footer </footer>
--- src/pages/HTML and CSS in depth/lab-form/LabForm.jsx
+++ src/pages/HTML and CSS in depth/lab-form/LabForm.jsx
@@ -3,35 +3,35 @@ import logo from "./logo.png";
 
 const LabForm = () => {
   return (
-    <div className="w-full">
-      <div className={styles.body}>
+    <div className="w-full flex justify-center bg-slate-50">
+      <div className={`${styles.body} w-[650px] px-4 py-2`}>
         <header>
           <img className={styles.img} alt="logo" src={logo} />
         </header>
         <main className={styles.main}>
           <form className={styles.form}>
-            <div>
+            <div className="py-2">
               <label for="email">Email:</label>
               <input
-                className={styles.input}
+                className={`${styles.input} ml-2 rounded-md p-1`}
                 type="email"
                 id="email"
                 name="email"
               />
             </div>
-            <div>
+            <div className="pb-2">
               <label for="date">Date:</label>
               <input
-                className={styles.input}
+                className={`${styles.input} ml-2 rounded-md p-1`}
                 type="date"
                 id="date"
                 name="date"
               />
             </div>
-            <div>
+            <div className="pb-2">
               <label for="numpeople">Number of people:</label>
               <input
-                className={styles.input}
+                className={`${styles.input} ml-2 rounded-md p-1`}
                 type="number"
                 id="numpeople"
                 min="1"
@@ -39,10 +39,10 @@ const LabForm = () => {
                 name="numpeople"
               />
             </div>
-            <div>
+            <div className="pb-2">
               <label for="agree">
                 <input
-                  className={styles.input}
+                  className={`${styles.input} mr-2 size-4`}
                   type="checkbox"
                   id="agree"
                   name="agree"
@@ -51,7 +51,12 @@ const LabForm = () => {
                 I agree to the cancellation policy
               </label>
             </div>
-            <button type="submit">Book Now</button>
+            <button
+              className="bg-emerald-800 text-white mt-2 py-2 px-4 rounded-lg hover:bg-white hover:text-emerald-800"
+              type="submit"
+            >
+              Book Now
+            </button>
           </form>
         </main>
       </div>
