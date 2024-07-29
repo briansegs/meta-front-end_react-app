c563300628799c3a23d7ea7b869cee339cbc095b
bsegers <briansegs@gmail.com>
2024-07-29 10:06:03 -0400

    Tweek styles

--- src/pages/Advanced React/week2/practice-state-1/PracticeState1.jsx
+++ src/pages/Advanced React/week2/practice-state-1/PracticeState1.jsx
@@ -7,36 +7,38 @@ function App() {
   });
 
   return (
-    <div className="m-0 flex place-items-center flex-col w-full min-w-[320px] min-h-[80vh] text-[#ffffffde] bg-[#242424] leading-[1.5] justify-center items-center font-normal">
-      <h1 className="text-[3.2em] leading-[1.1]">{`${greeting.greet} ${greeting.name}`}</h1>
-      <div className="p-[2em] flex flex-col items-center justify-center gap-4">
+    <div className="w-full flex justify-center bg-slate-100">
+      <div className="mt-10 flex place-items-center flex-col h-fit w-[550px] py-14 rounded-xl px-16 text-[#ffffffde] bg-[#242424] leading-[1.5] justify-center items-center font-normal">
+        <h1 className="text-[3.2em] leading-[1.1]">{`${greeting.greet} ${greeting.name}`}</h1>
+        <div className="p-[2em] flex flex-col items-center justify-center gap-4">
+          <button
+            className="rounded-lg border-[1px] border-transparent py-[0.6em] px-[1.2em] text-[1em] font-medium bg-[#1a1a1a] cursor-pointer duration-[0.25s] hover:border-[#646cff] focus:outline-4 "
+            onClick={() =>
+              setGreeting((prevGreeting) => ({
+                ...prevGreeting,
+                greet: "Sup",
+                name: "Bro",
+              }))
+            }
+          >
+            Change greeting
+          </button>
+          <p>Click the above to change the greeting.</p>
+        </div>
+        <p className="text-[#888]">Click below to rest the greeting.</p>
         <button
-          className="rounded-lg border-[1px] border-transparent py-[0.6em] px-[1.2em] text-[1em] font-medium bg-[#1a1a1a] cursor-pointer duration-[0.25s] hover:border-[#646cff] focus:outline-4 "
+          className="rounded-lg border-[1px] border-transparent py-[0.6em] px-[1.2em] text-[1em] font-medium bg-[#1a1a1a] cursor-pointer duration-[0.25s] hover:border-[#646cff] focus:outline-4 mt-4"
           onClick={() =>
             setGreeting((prevGreeting) => ({
               ...prevGreeting,
-              greet: "Sup",
-              name: "Bro",
+              greet: "Hell0 there",
+              name: "user",
             }))
           }
         >
-          Change greeting
+          Rest Greeting
         </button>
-        <p>Click the above to change the greeting.</p>
       </div>
-      <p className="text-[#888]">Click below to rest the greeting.</p>
-      <button
-        className="rounded-lg border-[1px] border-transparent py-[0.6em] px-[1.2em] text-[1em] font-medium bg-[#1a1a1a] cursor-pointer duration-[0.25s] hover:border-[#646cff] focus:outline-4 mt-4"
-        onClick={() =>
-          setGreeting((prevGreeting) => ({
-            ...prevGreeting,
-            greet: "Hell0 there",
-            name: "user",
-          }))
-        }
-      >
-        Rest Greeting
-      </button>
     </div>
   );
 }
