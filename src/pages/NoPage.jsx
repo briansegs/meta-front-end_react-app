c3f741156d73109ed3168780cf01d023ac938bca
bsegers <briansegs@gmail.com>
2024-07-29 09:41:05 -0400

    Add page styles

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -4,10 +4,17 @@ const NoPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>404</h1>
-      <h2>Page not found</h2>
-      <button onClick={() => navigate(-1)}>Go back</button>
+    <div className="w-full flex justify-center bg-slate-100">
+      <div className="bg-white size-fit mt-12 mx-4 mb-4 px-14 py-12 rounded-xl flex flex-col items-center border-[1px] border-slate-200">
+        <h1 className="text-6xl mb-2">404</h1>
+        <h2 className="text-4xl mb-4">Page not found</h2>
+        <button
+          className="border-2 text-xl px-4 py-2 rounded-xl hover:border-gray-300 hover:bg-gray-100"
+          onClick={() => navigate(-1)}
+        >
+          Go back
+        </button>
+      </div>
     </div>
   );
 };
