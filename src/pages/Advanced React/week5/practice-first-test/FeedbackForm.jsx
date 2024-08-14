0d2c933590bdc4a52060398ef9c66e6891745f26
bsegers <briansegs@gmail.com>
2024-08-14 16:39:41 -0400

    Add more tests

--- src/pages/Advanced React/week5/practice-first-test/FeedbackForm.jsx
+++ src/pages/Advanced React/week5/practice-first-test/FeedbackForm.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const defaultValues = {
   score: 5,
-  comments: "",
+  comment: "",
 };
 
 const FeedbackForm = ({ onSubmit }) => {
@@ -15,17 +15,21 @@ const FeedbackForm = ({ onSubmit }) => {
     }));
   };
 
-  const isDisabled = state.comments.length < 10 && state.score < 5;
+  const isDisabled = state.comment.length < 10 && state.score < 5;
 
   const placeholder = isDisabled
     ? "Please provide a comment explaining why the experience was not good. Minimum length is 10 characters."
     : "Optional feedback";
 
   const handleSubmit = (e) => {
-    const { score, comments } = state;
+    const { score, comment } = state;
     e.preventDefault();
-    onSubmit({ score, comments });
+    onSubmit({ score, comment });
   };
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <form
@@ -44,12 +48,13 @@ const FeedbackForm = ({ onSubmit }) => {
         onChange={handleOnChange}
       />
 
-      <label htmlFor="comments">Comments: </label>
+      <label htmlFor="comment">Comments: </label>
       <textarea
-        id="comments"
+        id="comment"
         placeholder={placeholder}
         className="border-2 border-gray-300 rounded-md p-1 h-[150px]"
         onChange={handleOnChange}
+        value={state.comment}
       />
 
       <button
