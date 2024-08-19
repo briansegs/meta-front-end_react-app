5efd8801d9c643a8915facaf836d5df7f395be89
bsegers <briansegs@gmail.com>
2024-08-19 13:17:08 -0400

    feat: Add card content

--- src/pages/Advanced React/week5/lab-create-portfolio/components/Card.jsx
+++ src/pages/Advanced React/week5/lab-create-portfolio/components/Card.jsx
@@ -7,7 +7,23 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack backgroundColor="white" rounded="xl">
+      <Image src={imageSrc} alt={title} rounded="xl" />
+      <VStack alignItems="left" padding="10px" spacing={5}>
+        <Heading textAlign="left" color="black" size="lg">
+          {title}
+        </Heading>
+        <Text color="black">{description}</Text>
+        <HStack>
+          <Text color="black" fontWeight="bold">
+            See more
+          </Text>
+          <FontAwesomeIcon color="black" icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Card;
