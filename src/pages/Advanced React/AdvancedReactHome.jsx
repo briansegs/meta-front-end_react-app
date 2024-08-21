b7fe60343cd508bc05f37f23046a802ec3fe527c
bsegers <briansegs@gmail.com>
2024-08-21 17:17:59 -0400

    Add certificate

--- src/pages/Advanced React/AdvancedReactHome.jsx
+++ src/pages/Advanced React/AdvancedReactHome.jsx
@@ -1,4 +1,7 @@
 import InfoCard from "../../components/InfoCard";
+import cert from "../../assets/cert.png";
+import certDownload from "../../assets/Coursera Advanced React.pdf";
+import { Link } from "react-router-dom";
 
 const learningList = [
   "1. Context Api for global State",
@@ -16,22 +19,45 @@ const AdvancedReactHome = () => {
         <h1 className="text-3xl font-medium text-slate-600">Advanced React</h1>
       </div>
 
-      <div className="w-full flex justify-center">
-        <InfoCard
-          title="First steps"
-          content={
-            <>
-              <p className="text-neutral-600 font-semibold text-xl">
-                Key learnings:
-              </p>
-              {learningList.map((idea) => (
-                <p key={idea} className="text-slate-600 pl-2">
-                  {idea}
+      <div className="section-styles">
+        <a href={certDownload} download="Brian-Meta-certificate">
+          <img alt="cert" src={cert} className="" />
+        </a>
+
+        <div className="section-row">
+          <InfoCard
+            title="First steps"
+            content={
+              <>
+                <p className="text-neutral-600 font-semibold text-xl">
+                  Key learnings:
                 </p>
-              ))}
-            </>
-          }
-        />
+                {learningList.map((idea) => (
+                  <p key={idea} className="text-slate-600 pl-2">
+                    {idea}
+                  </p>
+                ))}
+              </>
+            }
+          />
+
+          <InfoCard
+            title="Final Project"
+            content={
+              <div className="max-w-[30rem] flex items-center">
+                <p className="text-neutral-600 font-semibold text-xl">
+                  Project:
+                </p>
+                <Link
+                  to="/advanced-react/lab-create-portfolio"
+                  className="pl-2 text-red-400 hover:text-rose-300"
+                >
+                  Portfolio Link
+                </Link>
+              </div>
+            }
+          />
+        </div>
       </div>
     </section>
   );
