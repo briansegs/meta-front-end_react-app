edfc6e3b2000fcecd9c76001f0e36dd413d1284a
bsegers <briansegs@gmail.com>
2024-08-21 17:19:35 -0400

    refactor: Info card for new section styles

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,5 +1,5 @@
 const InfoCard = ({ title, content }) => (
-  <div className="m-8 w-fit rounded-lg border-[1px] border-white bg-slate-50">
+  <div className="w-full rounded-lg border-[1px] border-white bg-slate-50">
     <div className="flex items-center rounded-t-lg px-8 py-6 justify-center w-full bg-gradient-to-r from-blue-400 to-cyan-300">
       <p className="text-white text-3xl font-semibold">{title}</p>
     </div>
