c4ed2886d548e285cdb8368434b66dc11e8be3f0
bsegers <briansegs@gmail.com>
2024-08-30 10:10:19 -0400

    Add animation to button

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -6,7 +6,7 @@ export const HomeButton = () => {
     <Link to="/">
       <button className="text-left ml-4 flex gap-1 text-slate-700 hover:text-blue-500 group">
         <FaHome size={25} />{" "}
-        <span className="text-lg font-medium text-transparent group-hover:text-slate-700 transition">
+        <span className="text-lg font-medium translate-x-[-30px] text-transparent group-hover:translate-x-0  group-hover:text-slate-700 transition">
           Home
         </span>
       </button>
